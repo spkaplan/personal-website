fbf634d04634cbad542f8261a325ee2b7ab7c9a3
Sam Kaplan <samuelpkaplan@gmail.com>
2025-12-11 00:23:34 -0800

    smooth personal vs profession split

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,6 @@ export default function Header() {
             <div className={styles.logo}>Sam's Portfolio</div>
             <nav className={styles.nav}>
                 <a href="/" className={styles.navLink}>Home</a>
-                <a href="/personal" className={styles.navLink}>Personal</a>
-                <a href="/professional" className={styles.navLink}>Professional</a>
                 <a href="#" className={styles.navLink}>Contact</a>
             </nav>
         </header>
